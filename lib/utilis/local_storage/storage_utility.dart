9a09a60c1c600668c698fc466364b4b9a606d4a6
Pratham-Makwana <makwanapratham13@gmail.com>
2024-04-17 15:35:19 +0530

    Utilis Http,Validtion,Logger,Storag

---
+++ lib/utilis/local_storage/storage_utility.dart
@@ -0,0 +1,53 @@
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+
+class TLLocalStorage{
+  static final TLLocalStorage _instance = TLLocalStorage._internal();
+
+  factory TLLocalStorage(){
+    return _instance;
+  }
+
+  TLLocalStorage._internal();
+
+  final _storage = GetStorage();
+
+
+  // Generic method to save data
+  Future<void> saveData<T>(String key, T value) async {
+    await _storage.write(key, value);
+  }
+
+  // Generic method to read data
+  T? readData<T>(String key){
+    return _storage.read<T>(key);
+  }
+
+  // Generic method to remove data
+  Future<void> removeData(String key) async{
+    await _storage.remove(key);
+  }
+
+  // Clear all data in Storage
+  Future<void> ClearAll() async{
+      await _storage.erase();
+  }
+}
+
+/// *** *** *** *** *** Example *** *** *** *** *** ///
+
+/// LocalStorage localStorage = LocalStorage();
+///
+/// // Save data
+/// localStorage.saveData('username', 'JohnDoe');
+///
+/// // Read data
+/// String? username = localStorage.readData<String>('username');
+/// print('Username: $username'); // Output: Username: JohnDoe
+///
+/// // Remove data
+/// localStorage.removeData('username');
+///
+/// // Clear all data
+/// localStorage.clearAll();
---
+++ lib/utilis/validators/validation.dart
@@ -0,0 +1,68 @@
+
+import 'dart:html';
+
+import 'package:get_storage/get_storage.dart';
+
+class Tvalidator {
+  
+  static String? validateEmail(String? value){
+    if(value == null || value.isEmpty){
+      return 'Email is required.';
+    }
+
+    // Regular expression for email validation
+    final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    
+    if (!emailRegExp.hasMatch(value)){
+      return 'Invalid email address';
+    }
+    
+    return null;
+  }
+  
+  static String? validatePassword(String value){
+    if(value == null || value.isEmpty){
+      return 'Password is required';
+    }
+    
+    // Check for minimum password length
+    if(value.length < 6){
+      return 'Password must be at least 6 characters long';
+    }
+    
+    // check for uppercase letters
+    if(!value.contains(RegExp(r'[A-Z]'))){
+      return 'Password must contain at least one uppercase letter';
+    }
+    
+    // check for numbers
+    if(!value.contains(RegExp(r'[0-9]'))){
+      return 'Password must contain at least one number';
+    }
+    
+    
+    // check for special characters
+    if(!value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))){
+      return 'Password must contains at least one special characters.';
+    }
+
+    return null;
+  }
+  
+  static String? validatePhoneNumber(String? value){
+    if(value == null || value.isEmpty){
+      return 'Phone Number is required';
+    }
+
+    // Regular expression for phone number validation (assuming a 10-digits US phone number format)
+    final phoneRegExp = RegExp(r'^\d{10}$');
+
+    if(!phoneRegExp.hasMatch(value)){
+      return 'Invalid phone number format (10 digits required).';
+    }
+
+    return null;
+
+    /// Add more custom validators as needed for your specific requirements.
+  }
+}
