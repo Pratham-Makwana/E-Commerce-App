b5640f3d7deb4b59dcbdff5a7ca591ee4a0fddbb
Pratham-Makwana <makwanapratham13@gmail.com>
2024-04-20 23:56:37 +0530

    Signup Screen

---
+++ lib/features/authentication/screens/signup/signup.dart
@@ -0,0 +1,49 @@
+import 'package:e_commerce_app/common/widgets/login_signup/form_divider.dart';
+import 'package:e_commerce_app/common/widgets/login_signup/social_button.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/constants/text_strings.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_utils/get_utils.dart';
+import 'widgets/signup_form.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+
+      /// default appbar provide back arrow
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(TSizes.defaultSpace),
+
+          /// padding for the all the side form the screen
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /// Title
+              Text(
+                TTexts.signupTitle,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              SizedBox(height: TSizes.spaceBtwSection),
+
+              /// Form
+              const TSignupForm(),
+              SizedBox(height: TSizes.spaceBtwSection),
+
+              /// Divider
+              TFormDivider(dividerText: TTexts.orSignUpWith.capitalize!),
+              SizedBox(height: TSizes.spaceBtwSection),
+
+              /// Social Button
+              const TSocialButtons(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/authentication/screens/signup/widgets/signup_form.dart
@@ -0,0 +1,96 @@
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/constants/text_strings.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import 'terms_conditions_checkbox.dart';
+
+class TSignupForm extends StatelessWidget {
+  const TSignupForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          /// First & Last name
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: TTexts.firstName,
+                      prefixIcon: Icon(Iconsax.user)),
+                  expands: false,
+                ),
+              ),
+              SizedBox(width: TSizes.spaceBtwInputField),
+              Expanded(
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: TTexts.lastName,
+                      prefixIcon: Icon(Iconsax.user)),
+                  expands: false,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: TSizes.spaceBtwInputField),
+
+          /// Username
+          TextFormField(
+            decoration: const InputDecoration(
+                labelText: TTexts.username,
+                prefixIcon: Icon(Iconsax.user_edit)),
+            expands: false,
+          ),
+          SizedBox(height: TSizes.spaceBtwInputField),
+
+          /// Email
+          TextFormField(
+            decoration: const InputDecoration(
+                labelText: TTexts.email, prefixIcon: Icon(Iconsax.direct)),
+          ),
+          SizedBox(height: TSizes.spaceBtwInputField),
+
+          /// Phone Number
+          TextFormField(
+            decoration: const InputDecoration(
+                labelText: TTexts.phoneNo, prefixIcon: Icon(Iconsax.call)),
+          ),
+          SizedBox(height: TSizes.spaceBtwInputField),
+
+          /// Password
+          TextFormField(
+            obscureText: true,
+            decoration: const InputDecoration(
+              labelText: TTexts.password,
+              prefixIcon: Icon(Iconsax.password_check),
+              suffixIcon: Icon(Iconsax.eye_slash),
+            ),
+          ),
+          SizedBox(height: TSizes.spaceBtwInputField),
+
+          /// Term and Condition Checkbox
+          const TTermAndConditionCheckbox(),
+          SizedBox(
+            height: TSizes.spaceBtwSection,
+          ),
+
+          /// Sign UP Button
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                TTexts.createAccount,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
