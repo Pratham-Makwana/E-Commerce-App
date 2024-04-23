8b230ff25dc720f3f411d3c0ef8241202c2072fc
Pratham-Makwana <makwanapratham13@gmail.com>
2024-04-23 23:13:15 +0530

    Home CustomeShape To Container

--- lib/features/authentication/screens/home/home.dart
+++ lib/features/authentication/screens/home/home.dart
@@ -1,10 +1,51 @@
+import 'package:e_commerce_app/common/widgets/custom_shapes/container/circular_container.dart';
+import 'package:e_commerce_app/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            /// Wrap the widget that we want to apply Custom Shape on
+            ClipPath(
+              clipper: TCustomCurvedEdges(),
+              child: Container(
+                color: TColors.primary,
+                padding: const EdgeInsets.all(0),
+
+                /// we use stack widget for layout the widget top of each other
+                child: SizedBox(
+                  height: 400,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                          top: -150,
+                          right: -250,
+                          child: TCircularContainer(
+                            backgroundColor: TColors.white.withOpacity(0.1),
+                          )),
+                      Positioned(
+                          top: 100,
+                          right: -300,
+                          child: TCircularContainer(
+                            backgroundColor: TColors.white.withOpacity(0.1),
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
