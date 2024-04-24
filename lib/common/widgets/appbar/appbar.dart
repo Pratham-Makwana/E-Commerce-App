548b9b7f2754e8792c3b3afde84ad94ee70cb1d0
Pratham-Makwana <makwanapratham13@gmail.com>
2024-04-24 23:42:56 +0530

    Custom AppBar And ProductCart Menu Icon

---
+++ lib/common/widgets/appbar/appbar.dart
@@ -0,0 +1,48 @@
+import 'package:e_commerce_app/app.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/device/device_utility.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const TAppBar(
+      {super.key,
+      this.title,
+      this.showBackArrow = false,
+      this.leadingIcon,
+      this.actions,
+      this.leadingOnPress
+      });
+
+  final Widget? title;
+  final bool showBackArrow;
+  final IconData? leadingIcon;
+  final List<Widget>? actions;
+  final VoidCallback? leadingOnPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: TSizes.md),
+      child: AppBar(
+        automaticallyImplyLeading: false,
+        leading: showBackArrow
+            ? IconButton(
+                onPressed: () => Get.back(),
+                icon: const Icon(Iconsax.arrow_left))
+            //  If we don't want to show anything  Only want to show the title
+            : leadingIcon != null
+                ? IconButton(onPressed: leadingOnPress, icon: Icon(leadingIcon))
+                : null,
+        title: title,
+        actions: actions,
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(TDeviceUtils.getAppbarHeight());
+}
--- lib/features/authentication/screens/home/home.dart
+++ lib/features/authentication/screens/home/home.dart
@@ -1,19 +1,35 @@
+import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
 import 'package:e_commerce_app/common/widgets/custom_shapes/container/primary_header_container.dart';
+import 'package:e_commerce_app/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/text_strings.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:iconsax/iconsax.dart';
+
+import 'widgets/home_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
+            /// Header
             TPrimaryHeaderContainer(
-              child: Container(),
+              child: Column(
+                children: [
+                  /// AppBar
+                  THomeAppBar(),
+
+                  /// SearchBar
+                  /// Categories
+                ],
+              ),
             )
           ],
         ),
@@ -21,3 +37,7 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+
+
+
