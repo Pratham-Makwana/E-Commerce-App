0c8c13c2e0aa5eccf3cb2d33b7a8082458ca26a8
Pratham-Makwana <makwanapratham13@gmail.com>
2024-05-01 15:45:09 +0530

    Product Details Rating,Price,Title,Stocks,Brand & Share Button

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,6 +1,9 @@
-import 'package:e_commerce_app/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
-
 import 'package:flutter/material.dart';
+import 'package:e_commerce_app/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
+import 'package:e_commerce_app/features/shop/screens/product_details/widgets/product_meta_data.dart';
+import 'package:e_commerce_app/features/shop/screens/product_details/widgets/rating_share_widget.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+
 
 
 class ProductDetail extends StatelessWidget {
@@ -15,13 +18,29 @@ class ProductDetail extends StatelessWidget {
             /// 1 - Product Image Slider
             TProductImageSlider(),
 
-
             /// Product Details
+            Padding(
+              padding: EdgeInsets.only(
+                  right: TSizes.defaultSpace,
+                  left: TSizes.defaultSpace,
+                  bottom: TSizes.defaultSpace),
+              child: Column(
+                children: [
+                  /// ---- Rating & Share Button
+                  TRatingAndShare(),
+
+                  /// ---- Price , Title , Stock & Brand
+                  TProductMetaData(),
+                  /// ---- Attribute
+                  /// ---- Checkout Button-
+                  /// ---- Description
+                  /// ---- Review
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
---
+++ lib/features/shop/screens/product_details/widgets/product_meta_data.dart
@@ -0,0 +1,75 @@
+import 'package:e_commerce_app/common/widgets/custom_shapes/container/rounded_container.dart';
+import 'package:e_commerce_app/common/widgets/images/t_circular_images.dart';
+import 'package:e_commerce_app/common/widgets/texts/product_price_text.dart';
+import 'package:e_commerce_app/common/widgets/texts/product_title_text.dart';
+import 'package:e_commerce_app/common/widgets/texts/t_brand_title_text_with_verified_icon.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/enums.dart';
+import 'package:e_commerce_app/utils/constants/image_strings.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+
+
+
+class TProductMetaData extends StatelessWidget {
+  const TProductMetaData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        /// Price & Sales Price
+        Row(
+          children: [
+            /// Sales
+            TRoundedContainer(
+              radius: TSizes.sm,
+              backgroundColor: TColors.secondary.withOpacity(0.8),
+              padding: EdgeInsets.symmetric(
+                  horizontal: TSizes.sm, vertical: TSizes.xs),
+              child: Text(
+                '25%',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelLarge!
+                    .apply(color: TColors.black),
+              ),
+            ),
+            SizedBox(width: TSizes.spaceBtwItems,),
+
+            /// Price
+            Text('\$250',style: Theme.of(context).textTheme.titleSmall!.apply(decoration: TextDecoration.lineThrough),),
+            SizedBox(width: TSizes.spaceBtwItems / 2,),
+            const TProductPriceText(price: '175',isLarge: true,),
+          ],
+        ),
+        SizedBox(height: TSizes.spaceBtwItems / 1.5,),
+
+        /// Title
+        const TProductTitleText(title: 'Nike Sport TShirt'),
+        SizedBox(height: TSizes.spaceBtwItems / 1.5,),
+        /// Stock Status
+        Row(
+          children: [
+            const TProductTitleText(title: 'Status:'),
+            SizedBox(width: TSizes.spaceBtwItems / 1.5,),
+            Text('In Stock',style: Theme.of(context).textTheme.titleMedium  ,),
+          ],
+        ),
+        SizedBox(height: TSizes.spaceBtwItems / 1.5,),
+        
+        /// Brand
+        Row(
+          children: [
+            TCircularImage(image: TImages.shoeIcon,height: 32,width: 32,overlayColor: dark ? TColors.white : TColors.dark,),
+            const TBrandTitleWithVerifiedIcon(title: 'Nike',brandTextSize: TextSizes.medium,),
+          ],
+        )
+      ],
+    );
+  }
+}
