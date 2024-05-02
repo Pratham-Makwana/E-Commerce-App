d8abb6890e2cf9c33810f6b147796d801d73b2a6
Pratham-Makwana <makwanapratham13@gmail.com>
2024-05-02 16:06:38 +0530

    Botton Add To Cart For The Product Details Screen

---
+++ lib/features/shop/screens/product_details/widgets/botton_add_to_cart_widget.dart
@@ -0,0 +1,68 @@
+import 'package:e_commerce_app/common/widgets/icons/t_circular_icon.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TBottomAddToCart extends StatelessWidget {
+  const TBottomAddToCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Container(
+      padding: const EdgeInsets.symmetric(
+          horizontal: TSizes.defaultSpace, vertical: TSizes.defaultSpace / 2),
+      decoration: BoxDecoration(
+          color: dark ? TColors.darkerGrey : TColors.light,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(TSizes.cardRadiusLg),
+            topRight: Radius.circular(TSizes.cardRadiusLg),
+          )),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+               TCircularIcon(
+                 onPressed: (){},
+                icon: Iconsax.minus,
+                width: 40,
+                height: 40,
+                backgroundColor: TColors.darkGrey,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: TSizes.spaceBtwItems,
+              ),
+              Text(
+                '2',
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+              SizedBox(
+                width: TSizes.spaceBtwItems,
+              ),
+               TCircularIcon(
+                onPressed: (){},
+                icon: Iconsax.add,
+                width: 40,
+                height: 40,
+                backgroundColor: TColors.black,
+                color: Colors.white,
+              ),
+            ],
+          ),
+          ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.all(TSizes.md),
+                backgroundColor: TColors.black,
+                side: const BorderSide(color: TColors.black)
+              ),
+              child: const Text('Add To Cart'))
+        ],
+      ),
+    );
+  }
+}
