bdeb5fa796f6b4498b26c21821fdad4f76d6aaa9
Pratham-Makwana <makwanapratham13@gmail.com>
2024-05-02 16:03:51 +0530

    Product Details Page ChoiceChip For The Choice OF Diffrent Color & Size OF The Product

---
+++ lib/common/widgets/chips/choice_chip.dart
@@ -0,0 +1,42 @@
+import 'package:e_commerce_app/common/widgets/custom_shapes/container/circular_container.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class TChoiceChip extends StatelessWidget {
+  const TChoiceChip({
+    super.key,
+    required this.text,
+    required this.selected,
+    this.onSelected,
+  });
+
+  final String text;
+  final bool selected;
+  final void Function(bool)? onSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    final isColor = THelperFunctions.getColor(text) != null;
+    return Theme(
+      /// To make the Background Color to be Match to the system color we wrap the [ChoiceChip] Widget to Theme Widget
+      /// On the Property of the Theme canvasColor to the transparent
+      data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
+      child: ChoiceChip(
+        label: isColor ? const SizedBox() :  Text(text),
+        selected: selected,
+        onSelected: onSelected,
+        labelStyle: TextStyle(color: selected ? Colors.white : null),
+        avatar: isColor
+            ? TCircularContainer(
+                width: 50,
+                height: 50,
+                backgroundColor: THelperFunctions.getColor(text)!,) : null,
+        labelPadding: isColor ? const EdgeInsets.all(0) : null,
+        // Make icon in the center
+        padding: isColor ? const EdgeInsets.all(0) : null,
+        shape: isColor ? const CircleBorder() : null,
+        backgroundColor: isColor ? THelperFunctions.getColor(text)! : null,
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/product_details/widgets/product_attribute.dart
+++ lib/features/shop/screens/product_details/widgets/product_attribute.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/common/widgets/chips/choice_chip.dart';
 import 'package:e_commerce_app/common/widgets/custom_shapes/container/rounded_container.dart';
 import 'package:e_commerce_app/common/widgets/texts/product_price_text.dart';
 import 'package:e_commerce_app/common/widgets/texts/product_title_text.dart';
@@ -15,6 +16,7 @@ class TProductAttributes extends StatelessWidget {
     final dark = THelperFunctions.isDarkMode(context);
     return Column(
       children: [
+        /// --- Selected Attributes Pricing & Description
         TRoundedContainer(
           padding: const EdgeInsets.all(TSizes.md),
           backgroundColor: dark ? TColors.darkerGrey : TColors.grey,
@@ -82,11 +84,88 @@ class TProductAttributes extends StatelessWidget {
                   ),
                 ],
               ),
+
               /// Variation Details
-              const TProductTitleText(title: 'this is the description of the Product and it can go up to max 4 lines',maxLines: 4,smallSize: true,)
+              const TProductTitleText(
+                title:
+                    'this is the description of the Product and it can go up to max 4 lines',
+                maxLines: 4,
+                smallSize: true,
+              )
             ],
           ),
-        )
+        ),
+        SizedBox(
+          height: TSizes.spaceBtwItems,
+        ),
+
+        /// Attribute
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const TSectionHeading(
+              title: "Colors",
+              showActionButton: false,
+            ),
+            SizedBox(
+              height: TSizes.spaceBtwItems / 2,
+            ),
+            Wrap(
+              spacing: 8,
+
+              /// In The ChoiceChip in the horizonatal  they are not scrollable but They reached the end of the screen they move toward the new roe
+              children: [
+                TChoiceChip(
+                  text: 'Green',
+                  selected: true,
+                  onSelected: (value) {},
+                ),
+                TChoiceChip(
+                  text: 'Blue',
+                  selected: false,
+                  onSelected: (value) {},
+                ),
+                TChoiceChip(
+                  text: 'Yellow',
+                  selected: false,
+                  onSelected: (value) {},
+                ),
+              ],
+            )
+          ],
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const TSectionHeading(
+              title: "Sizes",
+              showActionButton: false,
+            ),
+            SizedBox(
+              height: TSizes.spaceBtwItems / 2,
+            ),
+            Wrap(
+              spacing: 8,
+              children: [
+                TChoiceChip(
+                  text: 'EU 34',
+                  selected: true,
+                  onSelected: (value) {},
+                ),
+                TChoiceChip(
+                  text: 'EU 36',
+                  selected: false,
+                  onSelected: (value) {},
+                ),
+                TChoiceChip(
+                  text: 'EU 38',
+                  selected: false,
+                  onSelected: (value) {},
+                ),
+              ],
+            )
+          ],
+        ),
       ],
     );
   }
