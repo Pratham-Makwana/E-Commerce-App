ae3e2f8d7b980cc59a46af4699e85d87d59b52e8
Pratham-Makwana <makwanapratham13@gmail.com>
2024-05-02 16:09:32 +0530

    Product Screen Choice Chip For Color & Size , Description Using readmore package & BottomBar For Add To Cart

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,42 +1,86 @@
+import 'package:e_commerce_app/common/widgets/texts/section_heading.dart';
+import 'package:e_commerce_app/features/shop/screens/product_details/widgets/botton_add_to_cart_widget.dart';
 import 'package:e_commerce_app/features/shop/screens/product_details/widgets/product_attribute.dart';
 import 'package:flutter/material.dart';
 import 'package:e_commerce_app/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
 import 'package:e_commerce_app/features/shop/screens/product_details/widgets/product_meta_data.dart';
 import 'package:e_commerce_app/features/shop/screens/product_details/widgets/rating_share_widget.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
-
-
+import 'package:iconsax/iconsax.dart';
+import 'package:readmore/readmore.dart';
 
 class ProductDetail extends StatelessWidget {
   const ProductDetail({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      bottomNavigationBar: const TBottomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
             /// 1 - Product Image Slider
-            TProductImageSlider(),
+            const TProductImageSlider(),
 
             /// Product Details
             Padding(
-              padding: EdgeInsets.only(
+              padding: const EdgeInsets.only(
                   right: TSizes.defaultSpace,
                   left: TSizes.defaultSpace,
                   bottom: TSizes.defaultSpace),
               child: Column(
                 children: [
                   /// ---- Rating & Share Button
-                  TRatingAndShare(),
+                  const TRatingAndShare(),
 
                   /// ---- Price , Title , Stock & Brand
-                  TProductMetaData(),
+                  const TProductMetaData(),
+
                   /// ---- Attribute
-                  TProductAttributes(),
-                  /// ---- Checkout Button-
+                  const TProductAttributes(),
+                  SizedBox(
+                    height: TSizes.spaceBtwSection,
+                  ),
+
+                  /// ---- Checkout Button
+                  SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          onPressed: () {}, child: const Text('Checkout'))),
+                  SizedBox(
+                    height: TSizes.spaceBtwSection,
+                  ),
+
                   /// ---- Description
+                  const TSectionHeading(
+                    title: 'Description',
+                    showActionButton: false,
+                  ),
+                  SizedBox(height: TSizes.spaceBtwItems),
+                  const ReadMoreText(
+                    'This is a Product description for Blue Nike Sleeve less vest. There are more things can be added but i am going to ingorer now because we can do when we doing backend work ',
+                    trimLines: 2,
+                    trimMode: TrimMode.Line,
+                    trimCollapsedText: 'Show more',
+                    trimExpandedText: 'Less',
+                    moreStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                    lessStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                  ),
+
                   /// ---- Review
+                  const Divider(),
+                  SizedBox(height: TSizes.spaceBtwItems,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const TSectionHeading(title: 'Review(199)',showActionButton: false),
+                      IconButton(onPressed: (){}, icon: const Icon(Iconsax.arrow_right_3,size: 18,))
+                    ],
+                  ),
+                  SizedBox(height: TSizes.spaceBtwSection,),
+
                 ],
               ),
             ),
