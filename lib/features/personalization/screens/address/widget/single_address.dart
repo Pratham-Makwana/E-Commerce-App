aa8baf9879f03f1484fc781e4a9806be5cb33f27
Pratham-Makwana <makwanapratham13@gmail.com>
2024-05-04 23:27:16 +0530

    User AddressScreen With And Add New Addresses

---
+++ lib/features/personalization/screens/address/widget/single_address.dart
@@ -0,0 +1,75 @@
+import 'package:e_commerce_app/common/widgets/custom_shapes/container/rounded_container.dart';
+import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class TSingleAddress extends StatelessWidget {
+  const TSingleAddress({super.key, required this.selectedAddress});
+
+  final bool selectedAddress;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return TRoundedContainer(
+      width: double.infinity,
+      padding: const EdgeInsets.all(TSizes.md),
+      showBorder: true,
+      backgroundColor: selectedAddress
+          ? dark
+              ? TColors.darkGrey
+              : TColors.primary.withOpacity(0.5)
+          : Colors.transparent,
+      borderColor: selectedAddress
+          ? Colors.transparent
+          : dark
+              ? TColors.darkerGrey
+              : TColors.grey,
+      margin: EdgeInsets.only(bottom: TSizes.spaceBtwItems),
+      child: Stack(
+        children: [
+          Positioned(
+            right: 5,
+            top: 0,
+            child: Icon(
+              selectedAddress ? Iconsax.tick_circle5 : null,
+              color: selectedAddress
+                  ? dark
+                      ? TColors.light
+                      : TColors.dark
+                  : null,
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'John Doe',
+                style: Theme.of(context).textTheme.titleLarge,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              const SizedBox(
+                height: TSizes.sm / 2,
+              ),
+              const Text(
+                '(+123) 456 7890',
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              const SizedBox(
+                height: TSizes.sm / 2,
+              ),
+              const Text(
+                '82356, Timmy Coves, South Liana, Maine, 87665, USA',
+                softWrap: true,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
