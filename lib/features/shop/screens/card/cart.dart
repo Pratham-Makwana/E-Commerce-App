f246eba18c1cc9c0efb64a71d6d93c6f0ec7d8c1
Pratham-Makwana <makwanapratham13@gmail.com>
2024-05-06 00:57:52 +0530

    Checkout Screen Coupon Widget

--- lib/features/shop/screens/card/cart.dart
+++ lib/features/shop/screens/card/cart.dart
@@ -1,9 +1,9 @@
 import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
-import 'package:e_commerce_app/common/widgets/products/cart/add_remove_button.dart';
-import 'package:e_commerce_app/common/widgets/products/cart/cart_item.dart';
-import 'package:e_commerce_app/common/widgets/texts/product_price_text.dart';
+import 'package:e_commerce_app/features/shop/screens/card/widgets/cart_items.dart';
+import 'package:e_commerce_app/features/shop/screens/checkout/checkout.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 
 class CartScreen extends StatelessWidget {
@@ -19,52 +19,13 @@ class CartScreen extends StatelessWidget {
           style: Theme.of(context).textTheme.headlineSmall,
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(TSizes.defaultSpace),
-          child: ListView.separated(
-            /// Error : Vertical Viewport wad given unbounded height.
-            /// To Avoid Error, In ListView remember to use shrinkWrap to true ,
-            /// So it only take space of his children widgets
-        
-            shrinkWrap: true,
-            /// Problem is when item is increased scrollview is not working because we are using two scrollview widget
-            /// [SingleChildScrollView] and [ListView] to over come the problem you can remove [SingleChildScrollView] or
-            /// add physics: const NeverScrollableScrollPhysics(), in [ListView].
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: 4,
-            separatorBuilder: (_, __) =>
-                SizedBox(height: TSizes.spaceBtwSection),
-            itemBuilder: (_, index) => Column(
-              children: [
-                const TCartItem(),
-                SizedBox(
-                  height: TSizes.spaceBtwItems,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    /// Extra Space
-                    Row(
-                      children: [
-                        SizedBox(width: 70,),
-                        TProductQuantityWithAddRemoveButton(),
-                      ],
-                    ),
-                    /// Add Remove Buttons
-        
-                    TProductPriceText(price: '256'),
-        
-                  ],
-                )
-              ],
-            ),
-          ),
-        ),
+      body: const Padding(
+        padding: EdgeInsets.all(TSizes.defaultSpace),
+        child: TCartItems(),
       ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(TSizes.defaultSpace),
-        child: ElevatedButton(onPressed: () {  }, child: const Text('Checkout \$256.0'),),
+        child: ElevatedButton(onPressed: () => Get.to(()=> const CheckoutScreen()), child: const Text('Checkout \$256.0'),),
       ),
     );
   }
