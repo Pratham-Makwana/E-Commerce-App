d294ceeaf133aa09bedef75bd106d16c454a2b3c
Pratham-Makwana <makwanapratham13@gmail.com>
2024-05-07 15:39:40 +0530

    HomeScreen Popular Categories's  Sub Categories

--- lib/features/shop/screens/home/widgets/home_categories.dart
+++ lib/features/shop/screens/home/widgets/home_categories.dart
@@ -1,5 +1,5 @@
 import 'package:e_commerce_app/common/widgets/image_text_widgets/vertical_image_text.dart';
-import 'package:e_commerce_app/features/shop/screens/sub_category.dart';
+import 'package:e_commerce_app/features/shop/screens/sub_category/sub_category.dart';
 import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
---
+++ lib/features/shop/screens/sub_category/sub_category.dart
@@ -0,0 +1,63 @@
+import 'package:e_commerce_app/common/widgets/appbar/appbar.dart';
+import 'package:e_commerce_app/common/widgets/images/t_rounded_images.dart';
+import 'package:e_commerce_app/common/widgets/products/product_cart/product_cart_horizontal.dart';
+import 'package:e_commerce_app/common/widgets/texts/section_heading.dart';
+import 'package:e_commerce_app/utils/constants/image_strings.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class SubCategoriesScreen extends StatelessWidget {
+  const SubCategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TAppBar(
+        showBackArrow: true,
+        title: Text(
+          'Sports',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Banner
+              const TRoundedImage(
+                imageUrl: TImages.banner3,
+                width: double.infinity,
+              ),
+              SizedBox(
+                height: TSizes.spaceBtwSection,
+              ),
+
+              /// Sub-Categories
+              Column(
+                children: [
+                  TSectionHeading(
+                    title: 'Sport shirts',
+                    onPressed: () {},
+                  ),
+                  SizedBox(
+                    height: TSizes.spaceBtwItems / 2,
+                  ),
+                  SizedBox(
+                    height: 120,
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 4,
+                        itemBuilder: (context, index) => const TProductCardHorizonatal(),
+                      separatorBuilder: (_,__) => SizedBox(width: TSizes.spaceBtwItems,),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
