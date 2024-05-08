607d920a0e7f9db412b98e739befc674f258cc6d
Pratham-Makwana <makwanapratham13@gmail.com>
2024-05-09 00:38:23 +0530

    Create Firebase Project And  Setup & Configure Firebase in App | Activate Firebase CLI & Firebase Services

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
+import 'package:e_commerce_app/data/repositories/authentication/authentication_repository.dart';
+import 'package:e_commerce_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'app.dart';
 
-void main(){
-
-  // Todo: Add Widgets Binding
+Future<void> main() async {
   // Todo: Init Local Storage
   // Todo: Await Native Splash
   // Todo: Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   // Todo: Initialise Authentication
   runApp(const App());
 }
-
-
