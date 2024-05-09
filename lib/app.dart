03497a20059cd7f43c6a483421ceb498ab04f68b
Pratham-Makwana <makwanapratham13@gmail.com>
2024-05-09 13:19:25 +0530

    Load and initialize GetStorage in main.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,12 @@
 
+import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:e_commerce_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import 'features/authentication/screens/OnBoarding/onboarding.dart';
 
+/// -- Use this Class to Setup theme, initial Binding, any animation and much more using Material widget
 class App extends StatelessWidget {
   const App({super.key});
 
@@ -15,7 +17,9 @@ class App extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
-      home: const OnBoardingScreen(),
+      //home: const OnBoardingScreen(),
+      /// Show Loader or Circular Progress Indicator meanwhile Authentication Repository is deciding to show relevant screen.
+      home:  const Scaffold(backgroundColor: TColors.primary,body: Center(child: CircularProgressIndicator(color: Colors.white,)),),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,20 @@ import 'package:get_storage/get_storage.dart';
 import 'app.dart';
 
 Future<void> main() async {
+  /// [ERROR:flutter/runtime/dart_vm_initializer.cc(41)] Unhandled Exception: Binding has not yet been initialized.
+  /// To Avoid Error Add Widgets Binding
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
   // Todo: Init Local Storage
+  /// -- GetX Local Storage
+  await GetStorage.init();
   // Todo: Await Native Splash
+  /// --- Await Splash until other items Load
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   // Todo: Initialize Firebase
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
   // Todo: Initialise Authentication
   runApp(const App());
 }
