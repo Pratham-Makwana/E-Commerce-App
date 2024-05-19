688783e481287826522072bdbb66cb9d207e6155
Pratham-Makwana <makwanapratham13@gmail.com>
2024-05-19 21:39:53 +0530

    Google Sign

--- lib/features/authentication/controllers/login/login_controller.dart
+++ lib/features/authentication/controllers/login/login_controller.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_app/data/repositories/authentication/authentication_repository.dart';
+import 'package:e_commerce_app/features/personalization/controllers/user_controller.dart';
 import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:e_commerce_app/utils/helpers/network_manager.dart';
 import 'package:e_commerce_app/utils/popups/full_screen_loader.dart';
@@ -17,14 +18,16 @@ class LoginController extends GetxController {
   final email = TextEditingController(); // Controller for email input
   final password = TextEditingController(); // Controller for password input
   GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+  final userController = Get.put(UserController());
 
   /// We Store the Data on the localStorage fot Fetch Data onInit Method is used
   @override
   void onInit() {
+    //email.text = localStorage.read('REMEMBER_ME_EMAIL');
+   // password.text = localStorage.read('REMEMBER_ME_PASSWORD');
     super.onInit();
     /// Assign The Latest value to Email and Password
-    email.text = localStorage.read('REMEMBER_ME_EMAIL');
-    password.text = localStorage.read('REMEMBER_ME_PASSWORD');
+
   }
 
   /// --Email and Password SignIn
@@ -48,12 +51,11 @@ class LoginController extends GetxController {
 
       // Save Data if Remember Me is selected
       if(rememberMe.value){
-        localStorage.write('REMEMBER_ME_EMAIL', email.text.trim());
+         localStorage.write('REMEMBER_ME_EMAIL', email.text.trim());
         localStorage.write('REMEMBER_ME_PASSWORD', password.text.trim());
       }
-
       // Login user using Email & Password Authentication
-      final userCredential = await AuthenticationRepository.instance.loginWithEmailAndPassword(email.text.trim(), password.text.trim());
+       await AuthenticationRepository.instance.loginWithEmailAndPassword(email.text.trim(), password.text.trim());
 
       // Remove The Loader
       TFullScreenLoader.stopLoading();
@@ -64,6 +66,36 @@ class LoginController extends GetxController {
       // Remove Loader
       TFullScreenLoader.stopLoading();
       // Show some Generic Error to the user
+      TLoaders.errorSnackBar(title: 'Oh Snap',message: e.toString());
+    }
+  }
+
+  /// --Google SignIn Authentication
+  Future<void> googleSignIn() async{
+    try{
+      // Start Loading
+      TFullScreenLoader.openLoadingDialog('Logging you in....', TImages.docerAnimation);
+
+      final isConnected = await NetworkManager.instance.isConnected();
+      if(!isConnected){
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+
+      // Google Authentication
+      final userCredentials = await AuthenticationRepository.instance.signInWithGoogle();
+
+      // Save User Record
+      await userController.saveUserRecord(userCredentials);
+
+      // Remove Loader
+      TFullScreenLoader.stopLoading();
+
+      // Redirect
+      AuthenticationRepository.instance.screenRedirect();
+    }catch(e){
+      // Remove Loader
+      TFullScreenLoader.stopLoading();
       TLoaders.errorSnackBar(title: 'Oh Snap',message: e.toString());
     }
   }
