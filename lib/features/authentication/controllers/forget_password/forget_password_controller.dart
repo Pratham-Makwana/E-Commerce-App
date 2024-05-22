da67b27f69c0d6e109a97c01aea720cb9a8575b3
Pratham-Makwana <makwanapratham13@gmail.com>
2024-05-22 22:01:05 +0530

    Forget Password Controller To sendPasswordResetEmail & resendPasswordResetEmail

---
+++ lib/features/authentication/controllers/forget_password/forget_password_controller.dart
@@ -0,0 +1,85 @@
+import 'package:e_commerce_app/data/repositories/authentication/authentication_repository.dart';
+import 'package:e_commerce_app/features/authentication/screens/login/login.dart';
+import 'package:e_commerce_app/features/authentication/screens/password_configuration/reset_password.dart';
+import 'package:e_commerce_app/utils/constants/image_strings.dart';
+import 'package:e_commerce_app/utils/helpers/network_manager.dart';
+import 'package:e_commerce_app/utils/popups/full_screen_loader.dart';
+import 'package:e_commerce_app/utils/popups/loaders.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ForgetPasswordController extends GetxController {
+  static ForgetPasswordController get instance => Get.find();
+
+  /// Variables
+  final email = TextEditingController();
+  GlobalKey<FormState> forgetPasswordFormKey = GlobalKey<FormState>();
+
+  /// Send Reset Password Email
+  sendPasswordResetEmail()  async {
+    try {
+      // Start Loading
+      TFullScreenLoader.openLoadingDialog('Processing Your Request', TImages.docerAnimation);
+
+      // Check Internet Connectivity
+      final isConnected = await NetworkManager.instance.isConnected();
+      if(!isConnected){
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+
+      // Form Validation
+      if(!forgetPasswordFormKey.currentState!.validate()){
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+
+
+      await AuthenticationRepository.instance.sendPasswordResetEmail(email.text.trim());
+
+      // Remove the Loader
+      TFullScreenLoader.stopLoading();
+
+      // Show Success Screen
+      TLoaders.successSnackBar(
+          title: 'Email Sent',
+          message: 'Email Link Sent to Reset Password ');
+
+      // Redirect
+      Get.to(() => ResetPasswordScreen(email: email.text.trim()));
+    } catch (e) {
+      // Remove Loader
+      TFullScreenLoader.stopLoading();
+      TLoaders.errorSnackBar(title: 'on Snap', message: e.toString());
+    }
+  }
+
+  resendPasswordResetEmail(String email) async {
+    try {
+      // Start Loading
+      TFullScreenLoader.openLoadingDialog(
+          'Processing Your Request', TImages.docerAnimation);
+
+      // Check Internet Connectivity
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+
+      await AuthenticationRepository.instance.sendPasswordResetEmail(email);
+
+      // Remove The Loader
+      TFullScreenLoader.stopLoading();
+
+      // Show Success Screen
+      TLoaders.successSnackBar(
+          title: 'Email Sent', message: 'Email Link Sent to Reset Email');
+    } catch (e) {
+      // Remove Loader
+      TFullScreenLoader.stopLoading();
+      TLoaders.errorSnackBar(title: 'On Snap', message: e.toString());
+    }
+  }
+}
