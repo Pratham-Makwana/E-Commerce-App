d6f05725f7247dbd3f16b272da83eda6fcd5bf21
Pratham-Makwana <makwanapratham13@gmail.com>
2024-05-22 22:02:09 +0530

    ResetPassword Screen For Resend the email

--- lib/features/authentication/screens/password_configuration/reset_password.dart
+++ lib/features/authentication/screens/password_configuration/reset_password.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce_app/features/authentication/controllers/forget_password/forget_password_controller.dart';
+import 'package:e_commerce_app/features/authentication/screens/login/login.dart';
 import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:e_commerce_app/utils/constants/text_strings.dart';
@@ -6,8 +8,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class ResetPassword extends StatelessWidget {
-  const ResetPassword({super.key});
+class ResetPasswordScreen extends StatelessWidget {
+  const ResetPasswordScreen({super.key, required this.email});
+
+  final String email;
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +20,14 @@ class ResetPassword extends StatelessWidget {
         automaticallyImplyLeading: false,
         actions: [
           IconButton(
-              onPressed: () => Get.back(), icon: const Icon(CupertinoIcons.clear))
+              onPressed: () => Get.back(),
+              icon: const Icon(CupertinoIcons.clear))
         ],
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(TSizes.defaultSpace),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Image(
                 image: const AssetImage(
@@ -36,6 +40,14 @@ class ResetPassword extends StatelessWidget {
               ),
 
               /// Title & SubTitle
+              Text(
+                email,
+                style: Theme.of(context).textTheme.bodyMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: TSizes.spaceBtwItems,
+              ),
               Text(
                 TTexts.changeYourPasswordTitle,
                 style: Theme.of(context).textTheme.headlineMedium,
@@ -50,22 +62,24 @@ class ResetPassword extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
               const SizedBox(
-                height: TSizes.spaceBtwItems,
+                height: TSizes.spaceBtwSection,
               ),
 
               /// Button
               SizedBox(
                   width: double.infinity,
                   child: ElevatedButton(
-                      onPressed: () {}, child: const Text(TTexts.done))),
+                      onPressed: () => Get.offAll(() => const LoginScreen()),
+                      child: const Text(TTexts.done))),
               const SizedBox(
                 height: TSizes.spaceBtwItems,
               ),
               SizedBox(
                   width: double.infinity,
                   child: TextButton(
-                      onPressed: () {}, child: const Text(TTexts.resendEmail))),
-
+                      onPressed: () => ForgetPasswordController.instance
+                          .resendPasswordResetEmail(email),
+                      child: const Text(TTexts.resendEmail))),
             ],
           ),
         ),
