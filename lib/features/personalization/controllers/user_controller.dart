b613f0f38dcc6d469e57a6c33436cfc494e25b78
Pratham-Makwana <makwanapratham13@gmail.com>
2024-05-26 23:12:28 +0530

    Fetch User Details

--- lib/features/personalization/controllers/user_controller.dart
+++ lib/features/personalization/controllers/user_controller.dart
@@ -1,15 +1,44 @@
 import 'package:e_commerce_app/data/repositories/user/user_repository.dart';
 import 'package:e_commerce_app/features/personalization/models/user_model.dart';
-import 'package:e_commerce_app/utils/popups/full_screen_loader.dart';
 import 'package:e_commerce_app/utils/popups/loaders.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class UserController extends GetxController {
   static UserController get instance => Get.find();
 
+  final profileLoading = false.obs;
+  Rx<UserModel> user = UserModel.empty().obs;
   /// Creating instance of the UserRepository
   final userRepository = Get.put(UserRepository());
+
+  final verifyEmail = TextEditingController();
+  final verifyPassword = TextEditingController();
+  final hiddenPassword = false.obs;
+  GlobalKey<FormState> reAuthFormKey = GlobalKey<FormState>();
+
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchUserRecord();
+  }
+
+
+  Future<void> fetchUserRecord() async {
+    try{
+      profileLoading.value = true;
+      final user = await userRepository.fetchUserDetails();
+      this.user(user);
+      profileLoading.value = false;
+    }catch(e){
+      user(UserModel.empty());
+    }finally{
+      profileLoading.value = false;
+    }
+  }
+
 
   /// Save user Record form any Registration Provider
   Future<void> saveUserRecord(UserCredential? userCredentials) async {
