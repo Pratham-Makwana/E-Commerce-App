f4458102ccf9e22a992b536bc30a2b4054adae8a
Pratham-Makwana <makwanapratham13@gmail.com>
2024-06-01 16:01:59 +0530

    Function For Close Account in Profile like deleteAccountWarningPopup,Delete User Account

--- lib/features/personalization/controllers/user_controller.dart
+++ lib/features/personalization/controllers/user_controller.dart
@@ -1,8 +1,15 @@
+import 'package:e_commerce_app/data/repositories/authentication/authentication_repository.dart';
 import 'package:e_commerce_app/data/repositories/user/user_repository.dart';
+import 'package:e_commerce_app/features/authentication/screens/login/login.dart';
 import 'package:e_commerce_app/features/personalization/models/user_model.dart';
+import 'package:e_commerce_app/features/personalization/screens/profile/widgets/re_authenticate_user_login.dart';
+import 'package:e_commerce_app/utils/constants/image_strings.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/helpers/network_manager.dart';
+import 'package:e_commerce_app/utils/popups/full_screen_loader.dart';
 import 'package:e_commerce_app/utils/popups/loaders.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class UserController extends GetxController {
@@ -10,14 +17,14 @@ class UserController extends GetxController {
 
   final profileLoading = false.obs;
   Rx<UserModel> user = UserModel.empty().obs;
+
   /// Creating instance of the UserRepository
   final userRepository = Get.put(UserRepository());
 
   final verifyEmail = TextEditingController();
   final verifyPassword = TextEditingController();
-  final hiddenPassword = false.obs;
+  final hiddenPassword = true.obs;
   GlobalKey<FormState> reAuthFormKey = GlobalKey<FormState>();
-
 
   @override
   void onInit() {
@@ -25,20 +32,18 @@ class UserController extends GetxController {
     fetchUserRecord();
   }
 
-
   Future<void> fetchUserRecord() async {
-    try{
+    try {
       profileLoading.value = true;
       final user = await userRepository.fetchUserDetails();
       this.user(user);
       profileLoading.value = false;
-    }catch(e){
+    } catch (e) {
       user(UserModel.empty());
-    }finally{
+    } finally {
       profileLoading.value = false;
     }
   }
-
 
   /// Save user Record form any Registration Provider
   Future<void> saveUserRecord(UserCredential? userCredentials) async {
@@ -46,9 +51,9 @@ class UserController extends GetxController {
       if (userCredentials != null) {
         // Convert Name to First and Last name
         final nameParts =
-            UserModel.nameParts(userCredentials.user!.displayName ?? '');
+        UserModel.nameParts(userCredentials.user!.displayName ?? '');
         final username =
-            UserModel.generateUsername(userCredentials.user!.displayName ?? '');
+        UserModel.generateUsername(userCredentials.user!.displayName ?? '');
 
         // Map Data
         final user = UserModel(
@@ -68,7 +73,79 @@ class UserController extends GetxController {
       TLoaders.warningSnackBar(
           title: 'Data Not Save',
           message:
-              'Something went wrong while saving your information.You can re-save data in your profile');
+          'Something went wrong while saving your information.You can re-save data in your profile');
+    }
+  }
+
+  /// Delete Account Warning
+  void deleteAccountWarningPopup() {
+    Get.defaultDialog(
+        contentPadding: const EdgeInsets.all(TSizes.md),
+        title: 'Delete Account',
+        middleText:
+        'Are you sure you want to delete your account permanently? This action is not reversible and all of your data will be removed permanently.',
+        confirm: ElevatedButton(
+          onPressed: () async => deleteUserAccount(),
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+              side: const BorderSide(color: Colors.red)),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: TSizes.lg),
+            child: Text('Delete'),
+          ),
+        ),
+        cancel: OutlinedButton(
+            onPressed: () => Navigator.of(Get.overlayContext!).pop(),
+            child: const Text('Cancel')));
+  }
+
+  /// Delete User Account
+  void deleteUserAccount() async {
+    try {
+      TFullScreenLoader.openLoadingDialog('Processing', TImages.docerAnimation);
+
+      /// First re-authenticate user
+      final auth = AuthenticationRepository.instance;
+      final provider = auth.authUser!.providerData.map((e) => e.providerId).first;
+      if (provider.isNotEmpty) {
+        // Re Verify Auth Email
+        if (provider == "google.com") {
+          await auth.signInWithGoogle();
+          await auth.deleteAccount();
+          TFullScreenLoader.stopLoading();
+          Get.offAll(() => const LoginScreen());
+        } else if (provider == 'password') {
+          TFullScreenLoader.stopLoading();
+          Get.to(() => const ReAuthLoginForm());
+        }
+      }
+    } catch (e) {
+      TFullScreenLoader.stopLoading();
+      TLoaders.warningSnackBar(title: 'Oh Snap!', message: e.toString());
+    }
+  }
+
+  Future<void> reAuthenticateEmailAndPasswordUser() async {
+    try {
+      TFullScreenLoader.openLoadingDialog('Processing', TImages.docerAnimation);
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+
+      if (!reAuthFormKey.currentState!.validate()) {
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+
+      await AuthenticationRepository.instance.reAuthenticateWithEmailAndPassword(verifyEmail.text.trim(), verifyPassword.text.trim());
+      await AuthenticationRepository.instance.deleteAccount();
+      TFullScreenLoader.stopLoading();
+      Get.offAll(() => const LoginScreen());
+    } catch (e) {
+      TFullScreenLoader.stopLoading();
+      TLoaders.warningSnackBar(title: 'Oh Snap!', message: e.toString());
     }
   }
 }
