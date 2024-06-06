670bb9d02c8939e46968220d61a44a09d5aa0288
Pratham-Makwana <makwanapratham13@gmail.com>
2024-06-06 13:26:10 +0530

    Upload Categories Dummy Data to Cloud Firebase

--- lib/data/repositories/categories/category_repository.dart
+++ lib/data/repositories/categories/category_repository.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_commerce_app/data/repositories/categories/firebase_storage_service.dart';
 import 'package:e_commerce_app/features/shop/models/category_model.dart';
 import 'package:e_commerce_app/utils/exceptions/firebase_exceptions.dart';
 import 'package:e_commerce_app/utils/exceptions/platform_exceptions.dart';
@@ -13,7 +14,7 @@ class CategoryRepository extends GetxController {
   final _db = FirebaseFirestore.instance;
 
   /// Get All Categories
-  Future<List<CategoryModel>?> getAllCategories() async {
+  Future<List<CategoryModel>> getAllCategories() async {
     try {
       // Get the add the add From the FirebaseFirestore
       final snapshot = await _db.collection('Categories').get();
@@ -33,4 +34,32 @@ class CategoryRepository extends GetxController {
   /// Get Sub Categories
 
   /// Upload Categories to Cloud Firebase
+  Future<void> uploadDummyData(List<CategoryModel> categories) async {
+    try {
+      //Upload all the Categories along with their Image
+      final storage = Get.put(FirebaseStorageService());
+
+      for (var category in categories) {
+        // Get ImageData link from the local assets
+        final file = storage.getImageDataFromAssets(category.image);
+
+        // Upload Image and Get its URl
+        final url = await storage.uploadImageData('Categories', file as Uint8List, category.name);
+
+        // Assign Url to Category. Image attribute
+        category.image = url;
+
+        // Store Category in Firestore
+        await _db.collection("Categories").doc(category.id).set(category.toJson());
+      }
+
+
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
+    }
+  }
 }
